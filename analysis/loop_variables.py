e83c4c39dc5b731e5a0c65b4c1dcf56629034e11
HelenCEBM <helencebm@gmail.com>
2021-11-23 14:40:52 +0000

    create variables to loop through for each test

---
+++ analysis/loop_variables.py
@@ -0,0 +1,69 @@
+# Import functions
+
+from cohortextractor import (
+    patients,
+    codelist,
+)
+
+# Import codelists
+
+from codelists import *
+
+
+def make_variable(code, index_date):
+    return {
+        # numeric value of each test
+        f"value_{code}": (
+            patients.with_these_clinical_events(
+                codelist([code], system="snomed"),
+                between=["index_date", "index_date + 6 days"],
+                returning="numeric_value",
+                find_last_match_in_period=True,
+                return_expectations={
+                    "incidence": 0.1,
+                    "float": {"distribution": "normal", "mean": 10, "stddev": 1},
+                },
+            )
+        ),
+        
+        # comparator ('<', '>', '=', etc) returned with test result
+        f"comparator_{code}": (
+            patients.comparator_from(
+                f"value_{code}",
+                return_expectations={
+                    "rate": "universal",
+                    "category": {
+                        "ratios": {  # ~, =, >= , > , < , <=
+                            None: 0.10,
+                            "~": 0.05,
+                            "=": 0.65,
+                            ">=": 0.05,
+                            ">": 0.05,
+                            "<": 0.05,
+                            "<=": 0.05}
+                    },
+                    "incidence": 0.80,
+                },
+            )
+        ),
+
+        # binary flag for comparators
+        f"comparator_flag_{code}":(
+            patients.satisfying(
+                f"comparator_{code}='<'",
+                return_expectations={
+                    "incidence": 0.1,
+                },
+            )
+        ),
+
+        # binary flag for each test (with numeric result)
+        f"flag_{code}":(
+            patients.satisfying(
+                f"value_{code}",
+                return_expectations={
+                    "incidence": 0.1,
+                },
+            )
+        )
+    }
