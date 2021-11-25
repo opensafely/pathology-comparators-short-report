1848e91a83e68a968cd564c6de470173f470d916
HelenCEBM <helencebm@gmail.com>
2021-11-25 08:11:05 +0000

    Additional measures

--- analysis/generate_measures.py
+++ analysis/generate_measures.py
@@ -5,9 +5,7 @@ from cohortextractor import Measure
 measures = list()
 
 for code in path_tests:
-    
     m = Measure(
-
         # rate of comparators being present for each test:
         id=f'{code}_comparator_rate',
         numerator=f"comparator_flag_{code}",
@@ -15,5 +13,39 @@ for code in path_tests:
         group_by="population",
         #small_number_suppression=apply_suppression
     )
-    
     measures.append(m)
+
+
+for code in path_tests:
+    m = Measure(
+        # rate of comparators being present for each test, split by region:
+        id=f'{code}_comparator_rate_by_region',
+        numerator=f"comparator_flag_{code}",
+        denominator=f"flag_{code}",
+        group_by="region",
+        #small_number_suppression=apply_suppression
+    )
+    measures.append(m)
+
+for code in path_tests:
+    m = Measure(
+        # rate of comparators being present for each test, split by comparator:
+        id=f'{code}_comparator_rate_by_comparator',
+        numerator=f"comparator_flag_{code}",
+        denominator=f"flag_{code}",  ###### could use smaller denominator here...
+        group_by=f"comparator_{code}",
+        #small_number_suppression=apply_suppression
+    )
+    measures.append(m)
+
+for code in path_tests:
+    m = Measure(
+        # rate of comparators being present for each test, split by numeric value:
+        id=f'{code}_comparator_rate_by_value',
+        numerator=f"comparator_flag_{code}",
+        denominator=f"flag_{code}",
+        group_by=f"value_{code}",
+        #small_number_suppression=apply_suppression
+    )
+    measures.append(m)
+
