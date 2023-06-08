d9f1972aaae376c1d5116ec35c22ec14208279d8
HelenCEBM <helencebm@gmail.com>
2023-06-08 10:05:14 +0100

    Adjust fit of axis labels in figure
    Also save all processed outputs to new folder for publication

--- analysis/data_presentation.py
+++ analysis/data_presentation.py
@@ -22,7 +22,10 @@ custom_sort = ["Glomerular filtration rate",
                 "Faecal calprotectin content"]
 
 BASE_DIR = Path(__file__).parents[1]
-OUTPUT_DIR = BASE_DIR / "output/released_outputs"
+INPUT_DIR = BASE_DIR / "output/released_outputs"
+OUTPUT_DIR = BASE_DIR / "publication_outputs" 
+if not os.path.exists(OUTPUT_DIR):
+   os.makedirs(OUTPUT_DIR)
 
 # import path test codelist to get code descriptions
 path_tests = pd.read_csv(os.path.join(f'codelists/user-helen-curtis-tests-with-comparators.csv')).set_index("code")
@@ -34,7 +37,7 @@ path_tests.loc[1020291000000106, "term"] = "GFR calculated by abbreviated MDRD"
 # # 1. comparator_rate_per_test
 #def simple_plot(factor=None):
 # # load data 
-df = pd.read_csv(os.path.join(OUTPUT_DIR, 'comparator_rate_per_test.csv'), index_col=0).sort_values(by="denominator", ascending=False)
+df = pd.read_csv(os.path.join(INPUT_DIR, 'comparator_rate_per_test.csv'), index_col=0).sort_values(by="denominator", ascending=False)
 df = df.join(path_tests)
 df.to_csv(os.path.join(OUTPUT_DIR, 'comparator_rate_per_test_with_names.csv'))
 
@@ -58,7 +61,7 @@ df.to_csv(os.path.join(OUTPUT_DIR, 'comparator_rate_per_test_with_names.csv'))
 def rate_breakdown(factor="region", indices=2):
     '''Produce a chart by tests and by specified factor (region, comparator,)'''
 
-    df = pd.read_csv(os.path.join(OUTPUT_DIR, f'comparator_rate_by_{factor}_per_test.csv'), index_col=[0,1])
+    df = pd.read_csv(os.path.join(INPUT_DIR, f'comparator_rate_by_{factor}_per_test.csv'), index_col=[0,1])
     df = df.stack().unstack(level=1).rename(columns={"flag":"total"})
     df = df.join(path_tests, on=None)
 
@@ -70,7 +73,7 @@ def rate_breakdown(factor="region", indices=2):
     df2 = df2.rename(columns={"level_1":factor})
     df2 = df2.set_index(["term",factor]).unstack()
     df2.columns = df2.columns.droplevel()
-    df2["max"] = df2.max(axis=1)
+    #df2["max"] = df2.max(axis=1)
 
     # sort tests:
     #df2 = df2.sort_values(by="max").drop("max", axis=1)
@@ -86,6 +89,8 @@ def rate_breakdown(factor="region", indices=2):
     plt.yticks(np.arange(0,len(df2.index)), df2.index, size=8)
     plt.colorbar()
 
+    plt.tight_layout()
+    plt.savefig(os.path.join(OUTPUT_DIR, f'comparator_rate_per_{factor}_per_test.png'))
     plt.show()
 
     # # count tests and number of different values for specified factor
@@ -118,20 +123,20 @@ def rate_breakdown(factor="region", indices=2):
     # plt.xlabel("Test code", size=20)
     # plt.ylabel("Proportion with comparators", size=16)
 
-    plt.savefig(os.path.join(OUTPUT_DIR, f'comparator_rate_per_{factor}_per_test.png'))
+    
 
-#rate_breakdown(factor="region")
+rate_breakdown(factor="region")
 
 
 ######### Comparators
-df = pd.read_csv(os.path.join(OUTPUT_DIR, f'comparator_rate_by_comparator_per_test.csv'), index_col=0)
+df = pd.read_csv(os.path.join(INPUT_DIR, f'comparator_rate_by_comparator_per_test.csv'), index_col=0)
 df = df.join(path_tests) # join test names
 df = df[["term", "<=_rate", ">=_rate"]]
 df.to_csv(os.path.join(OUTPUT_DIR, f'comparator_rate_by_comparator_per_test_with_names.csv'))
 
 
 ######### Values
-df = pd.read_csv(os.path.join(OUTPUT_DIR, f'comparator_rate_by_value_per_test.csv'), index_col=[0,1])
+df = pd.read_csv(os.path.join(INPUT_DIR, f'comparator_rate_by_value_per_test.csv'), index_col=[0,1])
 df = df.join(path_tests) # join test names
 
 # make two different output dfs (one for tests with mostly greater-than comparators, and one for less-than)
@@ -168,7 +173,7 @@ df_out_lt.to_csv(os.path.join(OUTPUT_DIR, f'all_non_gfr_tests_comparator_associa
 
 ######### Upper and lower limits (reference values returned alongside tests)
 for limit in ["upper", "lower"]:
-    df = pd.read_csv(os.path.join(OUTPUT_DIR, f'{limit}_bound_rate_per_test.csv'), index_col=[0,1])
+    df = pd.read_csv(os.path.join(INPUT_DIR, f'{limit}_bound_rate_per_test.csv'), index_col=[0,1])
     df = df.join(path_tests) # join test names
 
     # calculate rank for each limit value to find top 2 only
